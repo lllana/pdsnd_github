b4a442ba1a7f38f80d975645f20a4665ce2024f9
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-06-21 22:16:53 +0300

    add comments

--- Project_bikeshare.py
+++ Project_bikeshare.py
@@ -2,10 +2,12 @@ import time
 import pandas as pd
 import numpy as np
 
+# data used
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# aliases used
 GENDER_COL = "Gender"
 BIRTH_YEAR_COL = "Birth Year"   
 
